8151d6088abd0e2de2fba0f57e75acea2a40a51c
Raúl Reymundi <raul@kickstarter.com>
2022-08-14 19:23:06 -0400

    solution

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,14 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon}) {
+
+  const renderedCards = pokemon.map((poke) => <PokemonCard key={poke.id} pokemon={poke}/>)
+
   return (
     <Card.Group itemsPerRow={6}>
       <h1>Hello From Pokemon Collection</h1>
+      <div>{renderedCards}</div>
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [search, setSearch] = useState("")
+  const [pokemon, setPokemon] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then((r) => r.json())
+    .then((data) => setPokemon(data))
+  }, [])
+
+  const pokemonToDisplay = pokemon.filter((poke) => poke.name.toLowerCase().includes(search.toLocaleLowerCase()))
+
+  function handlePokemonAdd(newPokemon){
+    setPokemon([...pokemon, newPokemon])
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handlePokemonAdd}/>
       <br />
-      <Search />
+      <Search search={search} setSearch={setSearch}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemonToDisplay} />
     </Container>
   );
 }
